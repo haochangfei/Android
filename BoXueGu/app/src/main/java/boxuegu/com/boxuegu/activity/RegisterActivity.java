83dcc22961eb92d06c074d25acb8efe1fc630a9d
haochangfei <34223852+haochangfei@users.noreply.github.com>
2018-03-28 17:39:44 +0800

    201803281739

---
+++ BoXueGu/app/src/main/java/boxuegu/com/boxuegu/activity/RegisterActivity.java
@@ -0,0 +1,14 @@
+package boxuegu.com.boxuegu.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import boxuegu.com.boxuegu.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
