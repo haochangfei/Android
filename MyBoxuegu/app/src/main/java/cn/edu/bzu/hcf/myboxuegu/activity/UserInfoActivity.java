22f79686cc27826643781f72634178b41010a773
haochangfei <17862073318@163.com>
2018-05-23 17:50:31 +0800

    20180523
    第一次提交

--- MyBoxuegu/app/src/main/java/cn/edu/bzu/hcf/myboxuegu/activity/UserInfoActivity.java
+++ MyBoxuegu/app/src/main/java/cn/edu/bzu/hcf/myboxuegu/activity/UserInfoActivity.java
@@ -1,15 +1,60 @@
 package cn.edu.bzu.hcf.myboxuegu.activity;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import cn.edu.bzu.hcf.myboxuegu.R;
+import cn.edu.bzu.hcf.myboxuegu.bean.UserBean;
+import cn.edu.bzu.hcf.myboxuegu.utils.AnalysisUtils;
+import cn.edu.bzu.hcf.myboxuegu.utils.DBUtils;
 
-public class UserInfoActivity extends AppCompatActivity {
-
+public class UserInfoActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView tv_back;
+    private TextView tv_main_title;
+    private TextView tv_nickName,tv_signature,tv_user_Name,tv_sex;
+    private RelativeLayout rl_nivkName,rl_sex,rl_signature,rl_title_bar;
+    private String spUserName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        spUserName = AnalysisUtils.readLoginUserName(this);
+        init();
+        initData();
+        //setListenter();
+    }
+    private void init(){
+        tv_back = (TextView) findViewById(R.id.tv_back);
+        tv_main_title = (TextView) findViewById(R.id.tv_main_title);
+        tv_main_title.setText("个人资料");
+        rl_title_bar = (RelativeLayout) findViewById(R.id.title_bar);
+        rl_title_bar.setBackgroundColor(Color.parseColor("#30B4Fe"));
+        rl_nivkName = (RelativeLayout) findViewById(R.id.rl_nickName);
+        rl_sex = (RelativeLayout) findViewById(R.id.rl_sex);
+        rl_signature = (RelativeLayout) findViewById(R.id.rl_signature);
+        tv_nickName = (TextView) findViewById(R.id.tv_nickName);
+        tv_user_Name = (TextView) findViewById(R.id.tv_user_name);
+        tv_sex= (TextView) findViewById(R.id.tv_sex);
+        tv_signature = (TextView) findViewById(R.id.tv_signature);
+    }
+    private void initData(){
+        UserBean bean = new UserBean();
+        bean = DBUtils.getInstance(this).getUserInfo(spUserName);
+        if(bean == null){
+            bean = new UserBean();
+            bean.userName = spUserName;
+            bean.nickName = "问答精灵";
+            bean.sex = "男";
+        }
+    }
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- MyBoxuegu/app/src/main/java/cn/edu/bzu/hcf/myboxuegu/utils/DBUtils.java
+++ MyBoxuegu/app/src/main/java/cn/edu/bzu/hcf/myboxuegu/utils/DBUtils.java
@@ -2,6 +2,7 @@ package cn.edu.bzu.hcf.myboxuegu.utils;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import cn.edu.bzu.hcf.myboxuegu.bean.UserBean;
@@ -30,6 +31,27 @@ public class DBUtils {
         ContentValues cv = new ContentValues();
         cv.put("userName",bean.userName);
         cv.put("nickName",bean.nickName);
+        cv.put("sex",bean.sex);
+        cv.put("signature",bean.signature);
+        db.insert(SQLiteHelper.U_USERINFO,null,cv);
     }
-
+    public UserBean getUserInfo(String userName){
+        String sql = "SELECT * FROM " + SQLiteHelper.U_USERINFO + " WHERE userName=?";
+        Cursor cursor = db.rawQuery(sql,new String[ ]{userName});
+        UserBean bean = null;
+        while (cursor.moveToNext()){
+            bean = new UserBean();
+            bean.userName = cursor.getString(cursor.getColumnIndex("userName"));
+            bean.nickName = cursor.getString(cursor.getColumnIndex("nickName"));
+            bean.sex = cursor.getString(cursor.getColumnIndex("sex"));
+            bean.signature = cursor.getString(cursor.getColumnIndex("signature"));
+        }
+        cursor.close();
+        return bean;
+    }
+    public void updateUserInfo(String key,String value,String userName){
+        ContentValues cv = new ContentValues();
+        cv.put(key,value);
+        db.update(SQLiteHelper.U_USERINFO,cv,"userName=?",new String[]{userName});
+    }
 }
